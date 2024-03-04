695ffd0ab20b1ded8455934e09ff51390d7eba2c
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-03-04 18:38:27 +0300

    Week 1 - Mask Tracking System Homework

---
+++ Homeworks/MaskTrackingSystem/Workaround/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Business.Concrete;
+using Entities.Concrete;
+
+public class Program
+{
+    private static void Main(string[] args)
+    { 
+
+        Person person = new Person();
+        person.Name = "ENGİN";
+        person.LastName = "DEMİROĞ";
+        person.BirthDate = 1985;
+        person.IdentityNumber = 123;
+
+        PttManager pttManager = new(new PersonManager());
+        pttManager.GiveMask(person);
+
+    }
+
+    static void SelamVer()
+    {
+        Console.WriteLine("Merhaba");
+    }
+
+}
