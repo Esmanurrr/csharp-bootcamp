3bb736ae20323cd0eebb77c2cf6569cf17442caf
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-03-04 16:29:28 +0300

    Lecture 1 - Introduction

---
+++ Lectures/Intro/Intro/Business/CourseManager.cs
@@ -0,0 +1,32 @@
+﻿using Intro.Entities;
+
+namespace Intro.Business;
+
+public class CourseManager
+{
+    Course[] courses = new Course[2];
+    //constructor
+    public CourseManager()
+    {
+        Course course1 = new();
+        course1.Id = 1;
+        course1.Name = "C#";
+        course1.Description = ".NET 8";
+        course1.Price = 0;
+
+        Course course2 = new();
+        course2.Id = 1;
+        course2.Name = "Java";
+        course2.Description = "Java 17";
+        course2.Price = 10;
+
+        courses[0] = course1;
+        courses[1] = course2;
+
+    }
+
+    public Course[] GetAll()
+    {
+        return courses;
+    }
+}
---
+++ Lectures/Intro/Intro/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Intro.Business;
+using Intro.Entities;
+
+Console.WriteLine("Hello, World!");
+
+
+string message1 = "Krediler";
+int term = 12;
+double amount = 100000.5;
+
+//variables --> camelCase
+bool isAuthenticated = false;
+
+//condition 
+if (isAuthenticated)
+{
+    Console.WriteLine("Hoşgeldin Engin");
+}else
+{
+    Console.WriteLine("Sisteme giriş yap");
+}
+
+string[] loans = { "Kredi 1", "Kredi 2", "Kredi 3", "Kredi 4" };
+//string[] loans2 = new string[5];
+
+     //start    condition         increment
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+
+CourseManager courseManager = new();
+Course[] courses = courseManager.GetAll();
+
+for (int i = 0;i < courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Name + " / " + courses[i].Price);
+}
+
+
+Console.WriteLine("kod bitti");
